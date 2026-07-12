631d1d45a6767a3a7e7bd0dc309076a5321c534a
Jeff Mortada <mortada9190@gmail.com>
2026-07-13 01:02:03 +0300

    updates

---
+++ app/browser/chunk-43QRI7M6.js
@@ -0,0 +1,1 @@
+import{a as i}from"./chunk-7QPHHLS7.js";import{Q as e,ac as o}from"./chunk-ODJOP6QD.js";var g=(n,s)=>{let t=e(i),r=e(o);return t.isLoggedIn()||r.navigate(["/login"]),t.isLoggedIn()},d=(n,s)=>{let t=e(i),r=e(o);return t.isLoggedIn()||r.navigate(["/login"]),t.isAdmin()};export{g as a,d as b};
