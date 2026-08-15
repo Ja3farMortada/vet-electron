dc84e519162cd5854cba419dc5e2eb3c81f35d68
Jeff Mortada <mortada9190@gmail.com>
2026-08-15 23:20:12 +0300

    added balance sheet

--- app/browser/chunk-AXNYMEEO.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./chunk-MTQKROJO.js";import{d as o}from"./chunk-PXSL7A2C.js";import{ja as e}from"./chunk-TE3T3KIF.js";var g=(n,s)=>{let t=e(i),r=e(o);return t.isLoggedIn()||r.navigate(["/login"]),t.isLoggedIn()},d=(n,s)=>{let t=e(i),r=e(o);return t.isLoggedIn()||r.navigate(["/login"]),t.isAdmin()};export{g as a,d as b};
---
+++ app/browser/chunk-LO2TZEKO.js
@@ -0,0 +1,1 @@
+import{a as i}from"./chunk-X22LC62H.js";import{d as o}from"./chunk-UDWKR37T.js";import{ja as e}from"./chunk-TE3T3KIF.js";var g=(n,s)=>{let t=e(i),r=e(o);return t.isLoggedIn()||r.navigate(["/login"]),t.isLoggedIn()},d=(n,s)=>{let t=e(i),r=e(o);return t.isLoggedIn()||r.navigate(["/login"]),t.isAdmin()};export{g as a,d as b};
